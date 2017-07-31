1562efe35b8fc78c178726a39244aa745d73226d
Kate Studwell <katestudwell@gmail.com>
2017-07-31 18:43:25 -0400

    Get session attributes from request

--- web/controllers/alexa_controller.ex
+++ web/controllers/alexa_controller.ex
@@ -10,8 +10,7 @@ defmodule Suggestotron.AlexaController do
   end
   def intent_request(conn, "GetDifferentVenueIntent", request) do
     Logger.info "Processing GetDifferentVenues"
-    Logger.info request
-    search_results = conn |> fetch_session |> get_session(:search_results)
+    search_results = request[:session][:attributes][:search_results]
     {suggested_venue, remaining_venues} = AlexaResponses.get_venues(search_results)
     return_response(conn, suggested_venue, remaining_venues)
   end
